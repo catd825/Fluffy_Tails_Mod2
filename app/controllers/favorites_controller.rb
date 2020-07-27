1ff5c045ecfa5440e9e455840bcf895529043528
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-07-27 13:39:54 -0400

    Added fav controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,39 @@
 class FavoritesController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+    @favorite = Favorite.find(params[:id])
+  end
+
+  def new
+    @favorite = Favorite.new
+  end
+
+  def create
+    @favorite = Favorite.create(favorite_params)
+    redirect_to favorite_path(@favorite)
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:pet_id, :user_id)
+  end
+
+
 end
