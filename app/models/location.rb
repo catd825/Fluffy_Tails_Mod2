5f8c8392897a0c096057007550ee19cff1aa326b
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-07-27 11:38:16 -0400

    added forign keys to pets, but these needs to be changed

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,6 @@
 class Location < ApplicationRecord
+
+  has_many :pets
+  has_many :users, through: :pets
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  has_many :pets
+  has_many :locations, through: :pets
+
 end
