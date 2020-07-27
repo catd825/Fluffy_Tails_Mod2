29f95485218eeeed1c99789e641305ab4ff0bc1e
axiumone <axiumone@gmail.com>
2020-07-27 16:18:45 -0400

    pets_controller updated

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,5 @@
 class PetsController < ApplicationController
+    before_action :set_pet 
 
     def index
     end
@@ -6,5 +7,36 @@ class PetsController < ApplicationController
     def show
     end
 
-    def
+    def new
+        @pet = Pet.new
+    end
+
+    def create
+        @pet = Pet.create(pet_params)
+
+        if @pet.valid?
+            redirect_to pet_path(@pet)
+        else
+            redirect_to new_pet_path(@pet)
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+        @pet = Pet.destroy
+    end
+
+    private
+
+    def set_pet
+        @pet = Pet.find(params[:id])
+    end
+
+    def pet_params
+        params.require(:pet).permit(:name, :age, :breed, :animal_type)
+    end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,9 @@ class Pet < ApplicationRecord
   belongs_to :location
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
+
+  validates :name, presence: true
+  validates :breed, presence: true
+  validates :animal_type, presence: true
+  validates :age, presence: true
 end
