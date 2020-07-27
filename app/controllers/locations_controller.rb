64523f52f6b61c45e31de939a3e9c3da6202e042
catd825 <catd825@gmail.com>
2020-07-27 16:25:17 -0400

    locations update

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,56 @@
 class LocationsController < ApplicationController
+
+    def index 
+        @locations = Location.all
+    end
+
+    def show  
+        find_location
+    end
+
+    def new  
+        @location = Location.new
+    end 
+
+    def create
+        @location = Location.create(location_params)
+        
+
+        if @location
+            redirect_to locations_path  
+        else
+            redirect_to new_location_path
+        end
+    end
+
+    def edit 
+        find_location
+    end
+
+    def update
+        location = find_location.update(location_params)
+        if location  
+            redirect_to location_path(location)
+        else
+            redirect_to edit_location_path
+        end
+    end
+
+    def destroy 
+        find_location.destroy
+        redirect_to locations_path
+    end
+
+
+
+    private
+
+    def find_location 
+        @location = Location.find(params[:id])
+    end
+
+    def location_params 
+        params.require(:location).permit(:name, :address, :phone)
+    end
+
 end
