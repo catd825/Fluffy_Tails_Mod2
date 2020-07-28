7f7e9f3432b19ff57937a42036020989ddd8ae61
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-07-28 15:37:37 -0400

    commit

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -9,7 +9,8 @@ class Pet < ApplicationRecord
   validates :age, presence: true
 
   def self.search_by(search_term)
-    where("LOWER(name) LIKE :search_term OR LOWER(animal_type) LIKE :search_term", search_term: "%#{search_term.downcase}%")
+    where("LOWER(name) LIKE :search_term", search_term: "%#{search_term.downcase}%")
+      #where("LOWER(name) LIKE :search_term OR LOWER(animal_type) LIKE :search_term", search_term: "%#{search_term.downcase}%")
   end
 
 end
