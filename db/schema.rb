f123bd6bdb5e2a310cd5d788245c02db200a5329
catd825 <catd825@gmail.com>
2020-07-28 16:07:30 -0400

    commit issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_160700) do
+ActiveRecord::Schema.define(version: 2020_07_28_164955) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
