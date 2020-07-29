e48b7fa162fa7099f6e6e9ee0b4aad60ccdc8d54
catd825 <catd825@gmail.com>
2020-07-29 17:48:36 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@ Location.destroy_all
 User.create(name: Faker::Name.name, password: rand(123456..6123456).to_s, email:Faker::Internet.email)
 end
 
-20.times do
+5.times do
 Location.create(name: Faker::Address.city, address: Faker::Address.street_address, phone: rand(1234567890..9234567899))
 end
 
-20.times do
+40.times do
 Pet.create(name:Faker::Creature::Cat.name, age: rand(0..15), animal_type: Faker::Creature::Animal.name, breed: Faker::Creature::Dog.breed, img_url: "https://www.mercurynews.com/wp-content/uploads/2019/01/GettyImages-531306536.jpg", location_id: Location.all.sample.id)
 end
 
