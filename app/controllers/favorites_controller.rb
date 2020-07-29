5e01c1a8b442145ef0381ac24ca96796fba18c3f
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-07-29 17:39:50 -0400

    all

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,6 @@
 class FavoritesController < ApplicationController
 
-  before_action :set_pet, only: [:show, :update, :edit, :destroy]
+  before_action only: [:show, :update, :edit, :destroy, :new]
   skip_before_action :authorized, only: [:index, :show]
 
 
@@ -17,43 +17,17 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    favorite = Favorite.where(pet: Pet.find(params[:pet]), user: current_user)
-    if favorite == []
-      #create the favorite
-      Favorite.create(pet: Pet.find(params[:pet]), user: current_user)
-      @favorite_exists = true
-    else
-      #delete the favorite
-      favorite.destroy_all
-      @favorite_exists = false
-    end
-    respond_to do |format|
-      format.html {}
-      format.js {}
-    end
+    @current_user.favorites << Favorite.create(favorite_params)
+    redirect_to user_path(@current_user)
   end
 
   def update
-    favorite = Favorite.where(pet: Pet.find(params[:pet]), user: current_user)
-    if favorite == []
-      #create the favorite
-      Favorite.create(pet: Pet.find(params[:pet]), user: current_user)
-      @favorite_exists = true
-    else
-      #delete the favorite
-      favorite.destroy_all
-      @favorite_exists = false
-    end
-    respond_to do |format|
-      format.html {}
-      format.js {}
-    end
   end
 
   private
 
   def favorite_params
-    params.require(:favorite).permit(:user_id, :pet_id)
+    params.require(:favorite).permit( :pet_id)
   end
 
 
