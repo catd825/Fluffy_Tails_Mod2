c5b9f622865cd8a6e0d66d478cbc54cd3a24e4e0
catd825 <catd825@gmail.com>
2020-07-29 17:28:13 -0400

    fixed logout

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   patch '/sessions/reset_page', to: 'sessions#reset_page', as: 'session_reset'
   get '/sessions/new', to: 'sessions#new', as: 'new_session'
   post '/sessions', to: 'sessions#login', as: 'login'
-  delete '/sessions/logout', to: 'sessions#logout', as: 'logout'
-  
+  # delete '/sessions/logout', to: 'sessions#logout', as: 'logout'
+  get '/sessions/logout', to: 'sessions#logout', as: 'logout'
+
 end
