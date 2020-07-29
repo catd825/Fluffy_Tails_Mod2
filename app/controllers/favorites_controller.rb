9aa23d8e549c59c9d374ef39792dd97eb849de45
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-07-29 16:38:41 -0400

    updated user show page to show favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,22 +3,7 @@ class FavoritesController < ApplicationController
   before_action :set_pet, only: [:show, :update, :edit, :destroy]
   skip_before_action :authorized, only: [:index, :show]
 
-  def update
-    favorite = Favorite.where(pet: Pet.find(params[:pet]), user: current_user)
-    if favorite == []
-      #create the favorite
-      Favorite.create(pet: Pet.find(params[:pet]), user: current_user)
-      @favorite_exists = true
-    else
-      #delete the favorite
-      favorite.destroy_all
-      @favorite_exists = false
-    end
-    respond_to do |format|
-      format.html {}
-      format.js {}
-    end
-  end
+
 
 
 end
