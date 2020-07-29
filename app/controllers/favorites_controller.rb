c51e5daddef93edeb3609845e87e4ffcf0caf275
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-07-29 18:06:06 -0400

    semi favorites working

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < ApplicationController
 
-  before_action only: [:show, :update, :edit, :destroy, :new]
-  skip_before_action :authorized, only: [:index, :show]
+  #before_action only: [:show, :update, :edit, :destroy, :new]
+  skip_before_action :authorized, only: [:index, :show, :update, :edit, :destroy, :new]
 
 
   def index
