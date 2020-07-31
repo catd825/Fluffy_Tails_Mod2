24f284d2094c953e1ef2c23d227a0b937f318a63
catd825 <catd825@gmail.com>
2020-07-31 10:03:23 -0400

    changes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode
+  # geocoded_by :address
+  # after_validation :geocode
 
   has_many :pets, dependent: :destroy
 
